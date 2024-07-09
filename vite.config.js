619c8c9ca2190e189b3f670d249cb74aa110cd5a
Aryan <aryan23328@gmail.com>
2024-07-10 03:39:50 +0530

    second commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      // Add any specific rollup options here if needed
+    },
+  }
 })
