f11f4c794b028e6d1250d731df2b5548674d4ef5
AryanSi12 <145208787+AryanSi12@users.noreply.github.com>
2024-07-10 21:34:33 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    mimeTypes: {
-      'js': 'application/javascript'
-    }
-  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
   build: {
     outDir: 'dist', 
     assetsDir: 'assets',
